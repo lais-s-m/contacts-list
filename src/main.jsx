eb1feba8e67f5a396cde25251eb1acabd895d7d9
RBM Web <lais.martins@rbmweb.com.br>
2023-06-28 09:28:41 -0300

    App created

---
+++ src/main.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root, {loader as rootloader, action as rootAction} from './routes/root';
+import ErrorPage from './error-page';
+import Contact, {loader as contactLoader, action as contactAction} from './routes/contact';
+import EditContact, {action as editAction} from './routes/edit';
+import {action as destroyAction} from './routes/destroy';
+import Index from './routes';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    loader: rootloader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage/>,
+        children: [
+          {index: true, element: <Index/>},
+          {
+            path: "contacts/:contactId",
+            element: <Contact/>,
+            loader: contactLoader,
+            action: contactAction,
+          }
+        ]
+      },
+      
+      {index: true, element: <Index/>},
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact/>,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction
+      }
+    ]
+  },
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
